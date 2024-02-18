d1f77e4ec86a4840336040e6542f7a82197ed54c
derylxsofyan <87649593+DerylxSofyan@users.noreply.github.com>
2024-02-18 20:01:02 +0700

    Feature Customers

---
+++ app/Filament/Resources/ProductsResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductsResource\Pages;
+use App\Filament\Resources\ProductsResource\RelationManagers;
+use App\Models\Products;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductsResource extends Resource
+{
+    protected static ?string $model = Products::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('NamaProduk')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('Harga')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('Stok')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('NamaProduk')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('Harga')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('Stok')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProducts::route('/create'),
+            'edit' => Pages\EditProducts::route('/{record}/edit'),
+        ];
+    }
+}
